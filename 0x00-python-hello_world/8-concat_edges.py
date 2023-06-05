849ad0f243c55d58841bdb2a5b352a904415dfac
miahjerim <miahjerim@gmail.com>
2023-06-06 02:01:10 +0300

    new files created

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "python is an interpreted, interactive, object-oriented programming\
+ languge that combines remarkable power with very clear syntax"
+str = str[39:67] + str[106:111] + str[:6]
+print(str)
