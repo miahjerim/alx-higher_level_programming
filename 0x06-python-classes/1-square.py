5e92ac092ffb635a308f4198370e703bebb3be54
miahjerim <miahjerim@gmail.com>
2023-06-27 23:43:58 +0300

    README file created ,task 0, task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """A class representing an object with size attribute."""
+
+    def __init__(self, size):
+        """
+        Initialize the Square object with a given size.
+
+        Parameters:
+            size : The size of the object.
+        """
+        self.__size = size
